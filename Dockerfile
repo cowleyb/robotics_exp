64b33c3b383a320fc1bc42f76ea355a53f535a5a
ben <cowleyben23@gmail.com>
2026-04-28 18:49:15 +0800

    Added gazebo

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN apt-get update && apt-get upgrade -y
 RUN apt-get install -y python3-pip
 RUN apt-get install -y python3-colcon-common-extensions
 RUN apt-get install -y ros-$ROS_DISTRO-joint-state-publisher ros-$ROS_DISTRO-robot-state-publisher ros-$ROS_DISTRO-rviz2 ros-$ROS_DISTRO-xacro
+RUN apt-get install -y ros-$ROS_DISTRO-ros-gz ros-$ROS_DISTRO-ros-gz-bridge ros-$ROS_DISTRO-ros-gz-sim
 
 WORKDIR /workspace/ros2_ws
 
---
+++ ros2_ws/src/mentorpi_sim_gazebo/launch/spawn.launch.py
@@ -0,0 +1,58 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    description_path = get_package_share_directory("mentorpi_description")
+    gazebo_path = get_package_share_directory("ros_gz_sim")
+    sim_path = get_package_share_directory("mentorpi_sim_gazebo")
+
+    xacro_path = os.path.join(sim_path, "urdf", "mentorpi_gz.xacro")
+    world_path = os.path.join(sim_path, "worlds", "empty.world")
+
+    robot_description = Command(["xacro ", xacro_path])
+
+    return LaunchDescription(
+        [
+            SetEnvironmentVariable(
+                name="GZ_SIM_RESOURCE_PATH",
+                value=os.path.dirname(description_path),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(gazebo_path, "launch", "gz_sim.launch.py")
+                ),
+                launch_arguments={"gz_args": world_path}.items(),
+            ),
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                name="robot_state_publisher",
+                output="screen",
+                parameters=[{"robot_description": robot_description}],
+            ),
+            Node(
+                package="ros_gz_sim",
+                executable="create",
+                arguments=[
+                    "-topic",
+                    "robot_description",
+                    "-name",
+                    "mentorpi_a1",
+                    "-x",
+                    "0.0",
+                    "-y",
+                    "0.0",
+                    "-z",
+                    "0.1",
+                ],
+                output="screen",
+            ),
+        ]
+    )
